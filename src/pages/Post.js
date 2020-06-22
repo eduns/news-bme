d8881c640b216721d13346268a0f5569e0318112
Eduardo Nunes <edugenix@gmail.com>
2020-06-22 19:25:50 -0300

    tela de splash e hotfix na tela de Post

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 import store from '../utils/StoreData';
 
 export default function Post({ navigation }) {
     const post = navigation.getParam('post');
-    store.saveAccess(`user_${global.user.email}`, post)
     const isTrendingTopic = navigation.getParam('isTrendingTopic');
+    if(!isTrendingTopic) store.saveAccess(`user_${global.user.email}`, post);
     return <WebView style={{flex: 1}} source={{ uri: isTrendingTopic? post.webSearchUrl : post.url }} />
 }
